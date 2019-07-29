ead25bd4ac8f284bf88ab314c6a0ef4938a1a7cd
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2019-07-29 02:33:00 +0200

    fix ws connection

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :beet_diet, BeetDietWeb.Endpoint,
-  url: [scheme: "https", host: "beer-diet", port: 443],
+  url: [scheme: "https", host: "beer-diet.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
